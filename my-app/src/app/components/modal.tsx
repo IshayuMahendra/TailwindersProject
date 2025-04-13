638f454bbd8f6ad303591622bf0215a145916ced
Andrew Kendrick <andrew@kendrick.to>
2025-04-12 20:03:53 -0400

    Added animation to modal

--- my-app/src/app/components/modal.tsx
+++ my-app/src/app/components/modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { PropsWithChildren, ReactNode } from 'react';
+import React, { PropsWithChildren, ReactNode, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faX } from '@fortawesome/free-solid-svg-icons'
 
@@ -8,25 +8,41 @@ interface ModalProps {
     isShow: boolean;
     children?: ReactNode;
     onDismiss: Function;
+    transitionSeconds: number;
 };
 
-const Modal: React.FC<ModalProps> = ({ isShow, children, onDismiss }: ModalProps) => {
+const Modal: React.FC<ModalProps> = ({ isShow, children, onDismiss, transitionSeconds }: ModalProps) => {
+    const [modalOpacity, setmodalOpacity] = useState(0);
+    const [modalDisplay, setmodalDisplay] = useState(false);
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        if(init) {
+            if(isShow == true) {
+                setmodalDisplay(true);
+                setTimeout(() => setmodalOpacity(1), 100);
+            } else {
+                setmodalOpacity(0);
+                setTimeout(() => setmodalDisplay(false), transitionSeconds*1000);
+            }
+        }
+        setInit(true);
+    }, [isShow]);
 
     return (
         <>
-            {isShow &&
-                <div className="pol-modal-wrapper">
-                    <div className="pol-modal-overlay" onClick={() => onDismiss()}></div>
-                    <div className="pol-modal">
-                        <div className="pol-modal-header">
-                            <button className="pol-button ml-auto pol-btn-x pol-text-dark" onClick={() => onDismiss()}><FontAwesomeIcon icon={faX}/></button>
-                        </div>
-                        <div className="pol-modal-content">
-                            {children}
-                        </div>
-                    </div>
+        {modalDisplay &&
+            <div className="pol-modal-wrapper">
+            <div className="pol-modal-overlay" onClick={() => onDismiss()} style={{transitionDuration: transitionSeconds+"s", opacity: modalOpacity}}></div>
+            <div className="pol-modal" style={{transitionDuration: transitionSeconds+"s", opacity: modalOpacity}}>
+                <div className="pol-modal-header">
+                    <button className="pol-button ml-auto pol-btn-x pol-text-dark" onClick={() => onDismiss()}><FontAwesomeIcon icon={faX}/></button>
                 </div>
-            }
+                <div className="pol-modal-content">
+                    {children}
+                </div>
+            </div>
+        </div>
+        }
         </>
     );
 };
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -23,10 +23,10 @@ const SplashPage: React.FC = () => {
                 <p>your vote.  your polls.  our impact.</p>
                 <div className="mt-4 block">
                 <Link className="pol-button inline-block" href="/login">Login</Link>
-                <Link className="pol-button ml-4 inline-block" href="/login">Sign Out</Link>
+                <Link className="pol-button ml-4 inline-block" href="/login">Sign Up</Link>
                 </div>
             </div>
-            <Modal isShow={showModal} onDismiss={() => setShowModal(false)}>
+            <Modal isShow={showModal} onDismiss={() => setShowModal(false)} transitionSeconds={0.3}>
                 <div className="text-center">
                 <h2>about us</h2>
                 <p className="mt-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
