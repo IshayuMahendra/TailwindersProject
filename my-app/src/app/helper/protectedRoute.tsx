4f7099df4c362abae38a78135e4573758fbe7024
Andrew Kendrick <andrew@kendrick.to>
2025-04-15 08:26:08 -0400

    Implemented ProtectedRoute

---
+++ my-app/src/app/helper/protectedRoute.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useIsLoggedIn } from '../provider/loggedInProvider';
+
+interface ProtectedRouteProps {
+    children?: ReactNode;
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }: ProtectedRouteProps) => {
+    const router = useRouter();
+    const {isLoggedIn} = useIsLoggedIn();
+    useEffect(() => {
+        if(!isLoggedIn) {
+            router.push("/");
+        }
+    }, []);
+
+
+    return (
+        <>
+{isLoggedIn && {children}}
+        </>
+    );
+};
+
+export default ProtectedRoute;
