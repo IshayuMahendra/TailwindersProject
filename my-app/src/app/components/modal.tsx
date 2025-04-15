01511947b3055365d72c8c529526c5edb4734b03
Andrew Kendrick <andrew@kendrick.to>
2025-04-15 08:16:57 -0400

    Organized CSS; Fixed modal close btn

--- my-app/src/app/components/modal.tsx
+++ my-app/src/app/components/modal.tsx
@@ -7,11 +7,12 @@ import { faX } from '@fortawesome/free-solid-svg-icons'
 interface ModalProps {
     children?: ReactNode;
     bgColor?: string;
+    fgColor?: string;
     onDismiss: Function;
     transitionSeconds: number;
 };
 
-const Modal: React.FC<ModalProps> = ({ children, onDismiss, transitionSeconds, bgColor }: ModalProps) => {
+const Modal: React.FC<ModalProps> = ({ children, onDismiss, transitionSeconds, bgColor, fgColor }: ModalProps) => {
     const [modalOpacity, setmodalOpacity] = useState(0);
     useEffect(() => {
         setmodalOpacity(1);
@@ -28,7 +29,7 @@ const Modal: React.FC<ModalProps> = ({ children, onDismiss, transitionSeconds, b
             <div className="pol-modal-overlay" onClick={() => beginDismiss()} style={{transitionDuration: transitionSeconds+"s", opacity: modalOpacity}}></div>
             <div className="pol-modal" style={{transitionDuration: transitionSeconds+"s", opacity: modalOpacity, backgroundColor: bgColor ? bgColor: '#AAC789'}}>
                 <div className="pol-modal-header">
-                    <button className="pol-button ml-auto pol-btn-x pol-text-dark" onClick={() => beginDismiss()}><FontAwesomeIcon icon={faX}/></button>
+                    <button className="pol-button ml-auto pol-btn-x pol-text-dark" style={{color: fgColor ? fgColor: "#1E4147"}} onClick={() => beginDismiss()}><FontAwesomeIcon icon={faX}/></button>
                 </div>
                 <div className="pol-modal-content">
                     {children}
