9720d1cfe7363e60eb15a0405319e03e6b37899c
IshayuMahendra <rishayu@gmail.com>
2025-04-21 18:41:38 -0400

    added 404 page

---
+++ my-app/src/app/not-found.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import NavBar from "./components/navbar"
+import '@/app/styles/global_styles.css';
+import '@/app/styles/home.css';
+
+//This is the 404 page
+export default function NotFound() {
+    return (
+
+        <div className="home h-full flex flex-col ">
+            <NavBar />
+        
+    
+                <main className="ml-auto mr-auto flex-1 p-6 mt-80">
+                    <h1>404 NOT FOUND</h1>
+                </main>
+              
+
+        </div>
+
+    );
+}
+
