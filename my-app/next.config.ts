ec402ac6376067a2a0f37e3b5d6430fa18df6af8
Andrew Kendrick <andrew@kendrick.to>
2025-04-21 09:58:31 -0400

    Fixed deprecation warning on Next images conf

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["pollster.s3.us-east-005.backblazeb2.com"]
+    remotePatterns: [ {
+      protocol: 'https',
+      hostname: 'pollster.s3.us-east-005.backblazeb2.com',
+      pathname: '**',
+    }]
   }
 };
 
