6b1c726a0062a92d9d3daae0feaf98a6366df4b5
IshayuMahendra <rishayu@gmail.com>
2025-04-21 18:28:27 -0400

    added delete

--- my-app/src/app/components/pollList.tsx
+++ my-app/src/app/components/pollList.tsx
@@ -6,57 +6,59 @@ import Modal from './modal';
 import AddPollForm, { LocalPoll } from "./addPollForm";
 import { Poll } from "./addPollForm";
 
-
+//Main feed page that displaus all the polls
 const PollList: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
   const [editingPoll, setEditingPoll] = useState<Poll | null>(null);
-  
 
 
+  //State for the polls
   const [polls, setPolls] = useState<Poll[]>([
 
   ]);
 
+  //Function to create new polls
   const handleNewPoll = (poll: LocalPoll) => {
     const formData = new FormData();
     formData.append('question', poll.title);
     poll.options.forEach((option) => formData.append('option', option))
-    if(poll.image) {
+    if (poll.image) {
       formData.append('image', poll.image);
     }
-   fetch("http://localhost:3000/api/poll/create", {
+    fetch("http://localhost:3000/api/poll/create", {
       method: "POST",
       body: formData
     }).then(async (res: Response) => {
       const jsonData = await res.json();
-      if(res.status == 201) {
+      if (res.status == 201) {
         const poll: Poll = jsonData["poll"];
-          setPolls ((prev) => [...prev, poll])
-          setShowModal(false)
+        setPolls((prev) => [...prev, poll])
+        setShowModal(false)
       } else {
-       console.log("Unable to create poll");
+        console.log("Unable to create poll");
       }
     }).catch((error: Error) => {
-     console.log(error);
+      console.log(error);
     });
 
   };
 
+  //Function to edit the polls
   const handleEditPoll = (poll: LocalPoll) => {
     const formData = new FormData();
     formData.append('question', poll.title);
     poll.options.forEach((option) => formData.append('option', option))
-    if(poll.image) {
+    if (poll.image) {
       formData.append('image', poll.image);
     }
-   fetch(`http://localhost:3000/api/poll/${editingPoll?.id}`, {
+    fetch(`http://localhost:3000/api/poll/${editingPoll?.id}`, {
       method: "PUT",
       body: formData
     }).then(async (res: Response) => {
       const jsonData = await res.json();
-      if(res.status == 200) {
+      if (res.status == 200) {
         const poll: Poll = jsonData["poll"];
-        if(editingPoll){
+        if (editingPoll) {
           editingPoll.title = poll.title
           editingPoll.options = poll.options
           editingPoll.imageURL = poll.imageURL
@@ -64,14 +66,34 @@ const PollList: React.FC = () => {
         }
         setEditingPoll(null)
       } else {
-       console.log("Unable to create poll");
+        console.log("Unable to create poll");
       }
     }).catch((error: Error) => {
-     console.log(error);
+      console.log(error);
     });
 
   };
 
+  //Delete poll function 
+  const handleDeletePoll = (poll: Poll) => {
+    fetch(`http://localhost:3000/api/poll/${poll.id}`, {
+      method: 'DELETE'
+    })
+    .then (async( res: Response) => {
+      if(res.status == 200) {
+        const pollIndex = polls.indexOf(poll);
+        setPolls((prev) => {
+          let newPolls = [...prev] 
+          newPolls.splice(pollIndex, 1)
+          return newPolls;
+        })
+      }
+    
+    })
+
+  }
+
+  //Main central page
   return (
     <div className="h-[600px] w-full max-w-5xl mx-auto p-6 bg-[#17393F] text-[#ffffff] rounded-lg flex flex-col">
       <div className="flex-1 space-y-4 overflow-auto pr-1">
@@ -83,16 +105,17 @@ const PollList: React.FC = () => {
             <div className="mb-4">
               {poll.imageURL &&
                 <Image
-                src={poll.imageURL}
-                alt={`Image for poll ${index + 1}`}
-                width={150}
-                height={150}
-                className="rounded-md"
-              />
+                  src={poll.imageURL}
+                  alt={`Image for poll ${index + 1}`}
+                  width={150}
+                  height={150}
+                  className="rounded-md"
+                />
               }
             </div>
+
             <div>{poll.title}</div>
-              {/*Edit Button */}
+            {/*Edit Button */}
             <button
               className="mt-2 bg-[#355F63] hover:bg-[#43797F] text-white px-4 py-1 rounded"
               onClick={() => {
@@ -100,8 +123,18 @@ const PollList: React.FC = () => {
               }}
             >
               Edit
-    </button>
-            
+            </button>
+
+              {/*Delete button */}
+            <button
+              className="ml-4 mt-2 bg-[#355F63] hover:bg-[#43797F] text-white px-4 py-1 rounded"
+              onClick={() => {
+                handleDeletePoll(poll)
+              }}
+            >
+              Delete
+            </button>
+
           </div>
         ))}
       </div>
@@ -113,6 +146,7 @@ const PollList: React.FC = () => {
         <h2>+</h2>
       </button>
 
+        {/*This is the createa modal*/}
       {showModal && (
         <Modal
           onDismiss={() => setShowModal(false)}
@@ -125,6 +159,7 @@ const PollList: React.FC = () => {
           </div>
         </Modal>
       )}
+      {/*This is the editing modal */}
       {editingPoll && (
         <Modal
           onDismiss={() => setShowModal(false)}
