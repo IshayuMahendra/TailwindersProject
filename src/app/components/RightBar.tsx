c59a722d456e0ca9699c26df64ee0b003e12a31b
Andrew Kendrick <andrew@kendrick.to>
2025-04-23 12:36:29 -0400

    Used next image in RightBar

--- src/app/components/RightBar.tsx
+++ src/app/components/RightBar.tsx
@@ -5,59 +5,53 @@ import Image from "next/image";
 
 //Right side bar displaying the account options
 const RightSidebar: React.FC = () => {
-    /*Just added the basic image I saw online since I couldn't get the figma ones */
-    const contributors = [
-        { name: "John Doe", points: 15, image: "/img/avatar.webp" },
-        { name: "John Doe", points: 10, image: "/img/avatar.webp"  },
-        { name: "John Doe", points: 10, image: "/img/avatar.webp"  },
-        { name: "John Doe", points: 10, image: "/img/avatar.webp"  },
-    ];
+  interface TopUser {
+    userId: string;
+    displayName: string;
+    count: number;
+  }
 
-    interface TopUser {
-        userId: string;
-        displayName: string;
-        count: number;
-    }
+  const [topUsers, setTopUsers] = useState<TopUser[]>([]);
 
-    const [topUsers, setTopUsers] = useState<TopUser[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/stats", {
+      method: "GET"
+    }).then(async (res: Response) => {
+      const jsonData = await res.json();
+      if (res.status == 200) {
+        const topUsers: TopUser[] = jsonData;
+        setTopUsers(topUsers);
+      } else {
+        console.log("Unable to fetch top users");
+      }
+    }).catch((error: Error) => {
+      console.log(error);
+    });
+  }, []);
 
-    useEffect(() => {
-            fetch("http://localhost:3000/api/stats", {
-              method: "GET"
-            }).then(async (res: Response) => {
-              const jsonData = await res.json();
-              if (res.status == 200) {
-                const topUsers: TopUser[] = jsonData;
-                setTopUsers(topUsers);
-              } else {
-                console.log("Unable to fetch top users");
-              }
-            }).catch((error: Error) => {
-              console.log(error);
-            });
-    }, []);
-
-    return (
-        <aside className="pol-sidebar h-full w-full py-4 px-8 border-[#667753] border-l-0 lg:border-l-1">
-            <div className="text-md font-semibold mb-6 mt-2">Top Contributors</div>
-            <ul className="space-y-5">
-                {/*Mapping the array to a list -Ishayu */}
-                {topUsers.map((topUser, index) => (
-                    <li key={index} className="flex items-center space-x-4">
-                        <img
-                            src={"/img/avatar.webp"}
-                            alt={topUser.displayName}
-                            className="w-12 h-12 rounded-full object-cover border border-[#AAC789]"
-                        />
-                        <div className="flex flex-col">
-                            <span className="font-medium">{topUser.displayName}</span>
-                            <span className="text-sm text-[#88a27b]">{topUser.count} contribution(s)</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </aside>
-    );
+  return (
+    <aside className="pol-sidebar h-full w-full py-4 px-8 border-[#667753] border-l-0 lg:border-l-1">
+      <div className="text-md font-semibold mb-6 mt-2">Top Contributors</div>
+      <ul className="space-y-5">
+        {/*Mapping the array to a list -Ishayu */}
+        {topUsers.map((topUser, index) => (
+          <li key={index} className="flex items-center space-x-4">
+            <Image
+              src={"/img/avatar.webp"}
+              alt={topUser.displayName}
+              width={50}
+              height={50}
+              className="w-12 h-12 rounded-full object-cover border border-[#AAC789]"
+            />
+            <div className="flex flex-col">
+              <span className="font-medium">{topUser.displayName}</span>
+              <span className="text-sm text-[#88a27b]">{topUser.count} contribution(s)</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 export default RightSidebar;
