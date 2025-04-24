2a377468abae1aa908a5b774caeee61e7f241f66
Andrew Kendrick <andrew@kendrick.to>
2025-04-24 02:39:04 -0400

    Added login check to add poll button

--- src/app/components/pollList.tsx
+++ src/app/components/pollList.tsx
@@ -7,13 +7,14 @@ import { useUser } from "../provider/userProvider";
 import AddPollForm, { Poll } from "./addPollForm";
 import Modal from './modal';
 import PollCard from "./pollCard";
+import { useRouter } from "next/navigation";
 
 interface PollListProps {
-  collectionType: "profile"|"home"
+  collectionType: "profile" | "home"
 }
 
 //Main feed page that displaus all the polls
-const PollList: React.FC<PollListProps> = ({collectionType}) => {
+const PollList: React.FC<PollListProps> = ({ collectionType }) => {
   const [showModal, setShowModal] = useState(false);
 
   //State for the polls
@@ -21,11 +22,12 @@ const PollList: React.FC<PollListProps> = ({collectionType}) => {
 
   ]);
   const user = useUser();
+  const router = useRouter();
 
   const updatePolls = () => {
     let fetchURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/poll`
 
-    if(collectionType == "profile") {
+    if (collectionType == "profile") {
       fetchURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/profile`
     }
 
@@ -43,47 +45,56 @@ const PollList: React.FC<PollListProps> = ({collectionType}) => {
       console.log(error);
     });
   };
-  
+
   useEffect(updatePolls, [user.isLoggedIn]);
 
   //Main central page
   return (
     <>
-    <div className="w-full h-full bg-[#12282C] text-[#ffffff] flex flex-col items-center">
-      <div className="w-full md:w-2/3 lg:w-full xl:w-2/3 space-y-6 p-6">
-        {polls.map((poll) => (
-          <PollCard poll={poll} key={poll.id} onDelete={() => {
-            setPolls((prev) => prev.filter((entry) => entry.id != poll.id));
-          }}></PollCard>
-        ))}
-      </div>
-      <div className="flex w-full">
+      <div className="w-full h-full bg-[#12282C] text-[#ffffff] flex flex-col items-center">
+        <div className="w-full md:w-2/3 lg:w-full xl:w-2/3 space-y-6 p-6">
+          {polls.map((poll) => (
+            <PollCard poll={poll} key={poll.id} onDelete={() => {
+              setPolls((prev) => prev.filter((entry) => entry.id != poll.id));
+            }}></PollCard>
+          ))}
+        </div>
+        <div className="flex w-full">
           <div className="ml-auto">
-          <button
-            className="pol-button rounded-full text-center"
-            style={{position: "fixed", bottom: 0, marginBottom: 20, marginLeft: -100, width: 60, height: 60, fontSize: 20, padding: 0, border: 0}}
-            onClick={() => setShowModal(true)}
-          ><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
+            <button
+              className="pol-button rounded-full text-center"
+              style={{ position: "fixed", bottom: 0, marginBottom: 20, marginLeft: -100, width: 60, height: 60, fontSize: 20, padding: 0, border: 0 }}
+              onClick={() => {
+                if (!user.isLoggedIn) {
+                  const errorParams = new URLSearchParams();
+                  errorParams.set("login", "true");
+                  errorParams.set("error", "You must be logged in to do that.")
+                  router.push(`/home?${errorParams.toString()}`);
+                  return;
+                }
+                setShowModal(true)
+              }}
+            ><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
           </div>
         </div>
 
-      {/*This is the create modal*/}
-      {showModal && (
-        <Modal
-          onDismiss={() => setShowModal(false)}
-          transitionSeconds={0.3}
-          bgColor="#1E4147"
-          fgColor="#FFF"
-        >
-          <div className="pol-modal-large">
-            <AddPollForm onCompletion={(newPoll) => {
-              setPolls((prev) => [newPoll, ...prev])
-              setShowModal(false);
-            }}  />
-          </div>
-        </Modal>
-      )}
-    </div>
+        {/*This is the create modal*/}
+        {showModal && (
+          <Modal
+            onDismiss={() => setShowModal(false)}
+            transitionSeconds={0.3}
+            bgColor="#1E4147"
+            fgColor="#FFF"
+          >
+            <div className="pol-modal-large">
+              <AddPollForm onCompletion={(newPoll) => {
+                setPolls((prev) => [newPoll, ...prev])
+                setShowModal(false);
+              }} />
+            </div>
+          </Modal>
+        )}
+      </div>
     </>
   );
 };
