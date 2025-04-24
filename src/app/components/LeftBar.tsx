eea6dad02233a51afca99458dd9769f8c50945fc
Andrew Kendrick <andrew@kendrick.to>
2025-04-24 03:23:53 -0400

    added QR code for demo

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Link from "next/link";
+import { useState } from "react";
 import { useUser } from "../provider/userProvider";
 
 //Left side bar displaying all the contributors
@@ -13,21 +14,21 @@ const LeftSidebar: React.FC = () => {
     ];
 
     const user = useUser();
-
+    const [showQR, setShowQR] = useState(false);
     return (
         <aside className="pol-sidebar h-full w-full py-4 px-8 border-[#667753] border-r-0 lg:border-r-1">
             <ul className="space-y-3 mt-2">
                 {/*Mapping the array to a list -Ishayu */}
                 {navItems.map((item, index) => (
                     <li key={index}>
-                    {(!item.requiresLogin || user.isLoggedIn) &&
-                    <Link
-                        href={item.path}
-                        className="block py-2 px-3 rounded-md transition-colors duration-200 hover:bg-[#0E2A2D] hover:text-white"
-                    >
-                        {item.label}
-                    </Link>
-                    }
+                        {(!item.requiresLogin || user.isLoggedIn) &&
+                            <Link
+                                href={item.path}
+                                className="block py-2 px-3 rounded-md transition-colors duration-200 hover:bg-[#0E2A2D] hover:text-white"
+                            >
+                                {item.label}
+                            </Link>
+                        }
                     </li>
                 ))}
             </ul>
